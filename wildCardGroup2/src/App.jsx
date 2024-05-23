3012f2fca21c833a34f7b9283fcf629e3be9e41b
Vincent9567 <vrusso9567@gmail.com>
2024-05-23 18:53:32 -0400

    Thursday start of class

--- wildCardGroup2/src/App.jsx
+++ wildCardGroup2/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { PerspectiveCamera, Environment, OrbitControls } from "@react-three/drei";
+import { EffectComposer, HueSaturation } from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
+import { Landscape } from "./Landscape";
+import { SphereEnv } from "./SphereEnv";
+import { Airplane } from "./Airplane";
+import { Targets } from "./Targets";
+import { MotionBlur } from "./MotionBlur";
+import Cart from './Cart'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Cart />
+      {/* <SphereEnv />  */}
+      <Environment background={false} files={"assets/textures/envmap.hdr"} /> 
+      <OrbitControls />
+
+      <PerspectiveCamera makeDefault position={[0, 10, 10]} />
+
+      {/* <Landscape />   */}
+      {/* <Airplane /> 
+      <Targets /> */}
+
+       <directionalLight
+        castShadow
+        color={"#f3d29a"}
+        intensity={2}
+        position={[10, 5, 4]}
+        shadow-bias={-0.0005}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-near={0.01}
+        shadow-camera-far={20}
+        shadow-camera-top={6}
+        shadow-camera-bottom={-6}
+        shadow-camera-left={-6.2}
+        shadow-camera-right={6.4}
+      /> 
+
+      <EffectComposer>
+        <MotionBlur />
+        <HueSaturation
+          blendFunction={BlendFunction.NORMAL} // blend mode
+          hue={-0.15} // hue in radians
+          saturation={0.1} // saturation in radians
+        />
+      </EffectComposer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
