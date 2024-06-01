2457294b30a6bc5e66404c70754dafc667dfaaca
Matt Hemsworth <matthemsworth01@gmail.com>
2024-06-01 09:25:40 -0400

    add lap counter and display win message after 3 laps, also colliders from vincent

--- car-project/src/App.jsx
+++ car-project/src/App.jsx
@@ -1,21 +1,40 @@
 import { Canvas } from "@react-three/fiber";
-import { Scene } from './Scene';
-import { Physics } from "@react-three/cannon"
-import './App.css'
+import { Scene } from "./Scene";
+import { Physics } from "@react-three/cannon";
+import "./App.css";
+import { useState } from "react";
+import { Text3D } from "@react-three/drei";
 
 function App() {
+  const [lapCount, setLapCount] = useState(0);
+  const handleLapCount = () => {
+    setLapCount((lapCount) => lapCount + 1);
+  };
 
   return (
     <>
       <Canvas>
-        <Physics
-          broadphase="SAP"
-          gravity={[0, -2.6, 0]}>
-          <Scene />
+        <Physics broadphase="SAP" gravity={[0, -2.6, 0]}>
+          <Scene handleLaps={handleLapCount} />
         </Physics>
+        {lapCount > 3 && (
+          <Text3D
+            font="./assets/fonts/1.json"
+            position={[-10, 5, -30]}
+            scale={10}
+          >
+            You Win!
+            <meshNormalMaterial />
+          </Text3D>
+        )}
       </Canvas>
+      {lapCount >= 0 && lapCount <= 2 ? (
+        <h1>Lap Count: {lapCount}</h1>
+      ) : (
+        <h1>Final Lap</h1>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- car-project/src/Scene.jsx
+++ car-project/src/Scene.jsx
@@ -1,38 +1,55 @@
-import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei"
-import { Suspense, useEffect, useState } from 'react'
-import { Track } from "./Track"
-import { Kart } from "./Kart"
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import { Suspense, useEffect, useState } from "react";
+import { Track } from "./Track";
+import { Kart } from "./Kart";
+import BoxTrigger from "./WinSign";
 
-export function Scene() {
-    const [thirdPerson, setThirdPerson] = useState(true);
-    const [cameraPosition, setCameraPosition] = useState([-6, 3.9, 6.21])
+export function Scene({ handleLaps }) {
+  const [thirdPerson, setThirdPerson] = useState(true);
+  const [cameraPosition, setCameraPosition] = useState([-6, 3.9, 6.21]);
+  useEffect(() => {
+    function keydownHandler(e) {
+      if (e.key == "k") {
+        // random is necessary to trigger a state change
+        if (thirdPerson)
+          setCameraPosition([-6, 3.9, 6.21 + Math.random() * 0.01]);
+        setThirdPerson(!thirdPerson);
+      }
+    }
 
-    useEffect(() => {
-        function keydownHandler(e) {
-          if (e.key == "k") {
-            // random is necessary to trigger a state change
-            if(thirdPerson) setCameraPosition([-6, 3.9, 6.21 + Math.random() * 0.01]);
-            setThirdPerson(!thirdPerson); 
-          }
-        }
-    
-        window.addEventListener("keydown", keydownHandler);
-        return () => window.removeEventListener("keydown", keydownHandler);
-      }, [thirdPerson]);
+    window.addEventListener("keydown", keydownHandler);
+    return () => window.removeEventListener("keydown", keydownHandler);
+  }, [thirdPerson]);
+
 
   return (
     <>
       <Suspense fallback={null}>
-        <Environment files="./assets/textures/envmap.hdr" background blur={0.5} />
+        <Environment
+          files="./assets/textures/envmap.hdr"
+          background
+          blur={0.5}
+        />
 
-        <PerspectiveCamera makeDefault position={cameraPosition} fov={40} />
-        {!thirdPerson && (
-            <OrbitControls target={[-2.64, -0.71, 0.03]} />
-        )}
+        <PerspectiveCamera makeDefault position={cameraPosition} fov={100} />
+        {!thirdPerson && <OrbitControls target={[-2.64, -0.71, 0.03]} />}
 
         <Track />
+
         <Kart thirdPerson={thirdPerson} />
+        <BoxTrigger
+          args={[5, 1, 0.5]}
+          onCollideEnd={() => {
+            handleLaps();
+            console.log("lap");
+          }}
+          position={[9.5, 0.5, 1]}
+        />
       </Suspense>
     </>
-  )
+  );
 }
